50b5d96eede2db50db414f9320aab6019bfc9fd5
Jacob Canote <jacob@webdogs.com>
2017-02-02 23:41:40 -0800

    Minor version 2.3.8

--- webdogs-support.php
+++ webdogs-support.php
@@ -3,7 +3,7 @@
  * Plugin Name:       WEBDOGS Support + Maintenance
  * Plugin URI:        https://github.com/theWEBDOGS/webdogs-support
  * Description:       Support + Maintenance Configuration Tools: scheduled maintenance notifications, login page customizations, base plugin recommendations and more.
- * Version:           2.3.7
+ * Version:           2.3.8
  * Author:            WEBDOGS Support Team
  * Author URI:        WEBDOGS.COM
  * License:           GPL-2.0+
